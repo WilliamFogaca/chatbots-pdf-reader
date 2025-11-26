1a12f1e0dc2c295754bd8a9d956d793f84b4cb36
WilliamFogaca <williamps0810@gmail.com>
2025-11-26 15:39:46 -0300

    Initial commit web

---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-950">
+      <div className="flex h-full items-center justify-center">
+        <h2 className="font-bold text-2xl text-white">Hello world!</h2>
+      </div>
+    </div>
+  );
+}
